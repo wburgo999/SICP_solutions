e03ba84dbef98b286ed7872f34f2806befcce76a
wburgo <wburgo@wburgo-macbookpro.roam.corp.google.com>
2016-03-31 10:16:56 -0700

    Clean up test-lib

--- Will/test-lib.scm
+++ Will/test-lib.scm
@@ -1,7 +1,3 @@
-(define (disp . vs)
-     (for-each display vs)
-     (display "\n"))
-
 (define (assert-eq x y)
   (if (not (deep-equal? x y))
     (disp "Expected: " y " Actual: " x)))
@@ -11,18 +7,9 @@
     (disp x " is not within " band " of expected " y)))
 
 (define (assert-close x y)
-  (if (not (<
-             (abs (- x y))
+  (if (not (< (abs (- x y))
               .01))
     (disp "Result: " x " is not close to Expected: " y)))
-
-(define (disp-range f n)
-  (define (range counter)
-    (if (not (eq? counter n))
-      (begin
-        (disp (f counter))
-        (range (+ counter 1)))))
-  (range 1))
 
 (define (deep-equal? a b)
   (cond ((and (null? a) (null? b)) #t)
@@ -32,5 +19,10 @@
         ((and (not (pair? a)) (pair? b)) #f)
         ((and (not (pair? a)) (not (pair? b))) (eq? a b))
         (else (and (eq? (car a) (car b)) (deep-equal? (cdr a) (cdr b))))))
+
+; utility procedure that allows you to use display with arbitrary number of arguments
+(define (disp . elements)
+     (for-each display elements)
+     (display "\n"))
 
 
